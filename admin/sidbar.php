5ac61f347291ed71dcdcb26d791553c8d9ae86c5
prajwal <prajwalkotagi06@gmail.com>
2025-09-22 08:02:29 +0530

    sidebar update

--- admin/sidbar.php
+++ admin/sidbar.php
@@ -4,7 +4,7 @@
 <div class="shadow-lg w-[20%] max-w-md min-h-screen fixed left-0 top-0 bg-blue-500 text-white">
     <nav class="flex flex-col">
         <a href="view_user.php" class="py-3 px-4 hover:bg-blue-600">View user</a>
-        <a href="add_user.php" class="py-3 px-4 hover:bg-blue-600">Add user</a> <!-- New tab -->
+        <!-- <a href="add_user.php" class="py-3 px-4 hover:bg-blue-600">Add user</a>  -->
     </nav>
 </div>
 
